d50a586f84991919ded6724b69587f95c2fa747a
Thang0226 <thang.nd0226@gmail.com>
2024-09-13 09:45:35 +0700

    update files

--- bmi.js
+++ bmi.js
@@ -1,17 +1,20 @@
-let w = + document.getElementById("weight").value; 
-let h = + document.getElementById("height").value;
 
-let bmi = w / h**2
+function calculate() {
+    let w = + document.getElementById("weight").value; 
+    let h = + document.getElementById("height").value;
 
-if (bmi < 18) {
-    document.write("Underweight");
-}
-else if (bmi < 25.0) {
-    document.write("Normal");
-}
-else if (bmi < 30.0) {
-    document.write("Overweight");
-}
-else {
-    document.write("Obese");
+    let bmi = w / h**2
+
+    if (bmi < 18) {
+        document.write("Underweight");
+    }
+    else if (bmi < 25.0) {
+        document.write("Normal");
+    }
+    else if (bmi < 30.0) {
+        document.write("Overweight");
+    }
+    else {
+        document.write("Obese");
+    }
 }
